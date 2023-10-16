7cd15ce38f7dbad90a0bc81cb8652d52c476de2c
sanjiv576 <shresthasanjiv576@gmail.com>
2023-10-16 18:57:05 +0545

    add routes and path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import SingleProduct from './components/SingleProduct';
+import HomePage from './components/HomePage';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +22,15 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+
+      <Routes>
+        <Route path='/' element={<LandingPage />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/singleProduct/:productId' element={<SingleProduct />}></Route>
+        <Route path='/home' element={<HomePage />}></Route>
+      </Routes>
     </div>
   );
 }
