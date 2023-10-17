1cce29231c622820ced08394db32f0ee3726e6db
sanjiv576 <shresthasanjiv576@gmail.com>
2023-10-17 14:28:11 +0545

    add to cart feature, remaining to link with API

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -3,14 +3,17 @@ import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import dummyData from "../data/data";
 
+import { usePurchase } from "../utils/purchaseContext";
+
 import { useState } from "react";
 
 
 function SingleProduct() {
+    const purchase = usePurchase();
 
     const { productId } = useParams();
     const [product, setProduct] = useState({});
-    const [count, setCount] = useState(0);
+    const [quantity, setQuantity] = useState(0);
 
 
 
@@ -26,19 +29,47 @@ function SingleProduct() {
     }, [productId]);
 
     const hanldeIncrement = () => {
-        setCount(count + 1);
-        if (count >= 8) {
-            setCount(8);
+        setQuantity(quantity + 1);
+        if (quantity >= 8) {
+            setQuantity(8);
         }
     };
 
     const hanldeDecrement = () => {
 
-        setCount(count - 1);
+        setQuantity(quantity - 1);
 
-        if (count <= 0) {
-            setCount(0);
+        if (quantity <= 0) {
+            setQuantity(0);
         }
+    };
+
+    const handleAddToCart = (e) => {
+        e.preventDefault();
+        if (quantity === 0) return window.alert('Please, select quantity first');
+
+        const purchaseProudct = {
+            "name": product.name,
+            "quantity": quantity,
+            "price": product.price,
+        }
+      
+        console.log(`Selected purchase product name is : ${purchaseProudct.name}`);
+        console.log(`Selected purchase product quantity is : ${purchaseProudct.quantity}`);
+        console.log(`Selected purchase product price is : ${purchaseProudct.price}`);
+
+        // add in the purchaseContext
+        purchase.setPurchase([...purchase.purchase, purchaseProudct]);
+
+        console.log(`Length of purchaseContext is : ${purchase.purchase.length}`)
+
+
+        // print the data that storeing in the purchaseContext
+        // console.log(`Purchase product name is : ${purchase[0].name}`);
+
+        console.log("Add to cart");
+
+
     };
 
     return (
@@ -50,11 +81,10 @@ function SingleProduct() {
             <p>{product.description}</p>
 
             <button onClick={hanldeDecrement}>-</button>
-            {/* <span>{count}</span> */}
-            Quantity: {count}
+            Quantity: {quantity}
             <button onClick={hanldeIncrement}>+</button>
             <br />
-            <button>Add to Cart</button>
+            <button onClick={handleAddToCart}>Add to Cart</button>
 
 
 
