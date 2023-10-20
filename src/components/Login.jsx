f86a7de6e192d0917d4d0a7afaeccafa13c5c8c3
sanjiv576 <shresthasanjiv576@gmail.com>
2023-10-20 15:51:02 +0545

    decorate UIs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,75 +1,243 @@
 
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../utils/authContext';
 import { useUser } from '../utils/userContext';
 import userServices from '../services/userService';
+import { Alert, Avatar, Chip, Grid, Snackbar, } from '@mui/material';
+import { isEmail } from '../lib/input-validation';
+
+
 function Login() {
     const auth = useAuth();
     const user = useUser();
     const navigate = useNavigate();
     const [credentials, setCredentials] = useState({ email: "", password: "" });
-
+    // for counting the number of times wrong credentials are attempted
     const [counter, setCounter] = useState(0);
 
+    // for open and close snackbar
+    const [open, setOpen] = React.useState(false);
+
+    // for closing snackbar
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
+
+    // does any input field has error either true or false
+    const [emailError, setEmailError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+
+    // handle error
+    const [formError, setFormError] = useState("");
+    const [formWarning, setFormWarning] = useState("");
 
 
+    // for changing blur color of the input field
+    const handleEmailError = () => {
+        if (!credentials.email || !isEmail(credentials.email)) {
+            setEmailError(true);
+            setFormError("Please, enter a valid email address.");
+            return;
+        }
+        setEmailError(false);
+        setFormError("");
+    };
+    // for changing blur color of the input field
+    const handlePasswordError = () => {
+        if (!credentials.password) {
+            setPasswordError(true);
+            setFormError("Please, enter a valid password.");
+            return;
+        }
+        setPasswordError(false);
+        setFormError("");
+    };
     const handleLogin = (e) => {
         e.preventDefault();
+
+        setFormError("");
+        setFormWarning("");
 
         console.log(`Email: ${credentials.email}`);
         console.log(`Password: ${credentials.password}`);
 
-        userServices.login(credentials)
-            .then(res => {
-                // set the user as logged in
-                auth.setEmail(credentials.email);
+        if (credentials.email === "" || credentials.password === "") {
+            setFormError("Please, enter all the fields.");
+            return;
+        }
 
-                //   get the token from the response and save in the local storage
-                window.localStorage.setItem('token', res.data.token);
+        if (credentials.email !== "" && credentials.password !== "") {
+            userServices.login(credentials)
+                .then(res => {
+                    // set the user as logged in
+                    auth.setEmail(credentials.email);
 
-                // store the user in the user context
-                user.setUser(res.data.user);
+                    //   get the token from the response and save in the local storage
+                    window.localStorage.setItem('token', res.data.token);
 
-                // naviagte to the home page if it is user , otherwise to the admin profile
-                if (res.data.user.role === 'user') {
-                    navigate('/home');
-                }
-                else if (res.data.user.role === 'admin') {
-                    navigate('/adminProfile');
-                }
-            })
-            .catch(err => {
-                window.alert(err.response.data.error);
+                    // store the user in the user context
+                    user.setUser(res.data.user);
 
-                // if the account is not registered, then redirect to the register page
-                if (err.response.data.error == 'Account has not been registered.') {
-                    setCounter(counter + 1);
-                }
+                    // naviagte to the home page if it is user , otherwise to the admin profile
+                    if (res.data.user.role === 'user') {
+                        navigate('/home');
+                    }
+                    else if (res.data.user.role === 'admin') {
+                        navigate('/adminProfile');
+                    }
+                })
+                .catch(err => {
+                    // window.alert(err.response.data.error);
+                    setFormError(err.response.data.error);
+                    setOpen(true);
 
-                // if wrong credentials are attempted more than 3 times, then lock the account
-                if (counter === 3) {
-                    // lock the account
-                    userServices.lockAccount({ email: credentials.email })
-                        .then(res => {
-                            if (res.data.status === 'disable') {
-                                window.alert('Your account has been locked. Please, contact the admin to unlock your account.');
-                            }
-                            // reset the counter
-                            setCounter(0);
-                        })
-                        .catch(err => {
-                            window.alert(err.response.data.error);
-                        });
+                    // if the account is not registered, then redirect to the register page
+                    if (err.response.data.error == 'Account has not been registered.') {
+                        setCounter(counter + 1);
+                    }
 
-                }
-                // 
-            });
+                    // if wrong credentials are attempted more than 3 times, then lock the account
+                    if (counter === 3) {
+                        // lock the account
+                        userServices.lockAccount({ email: credentials.email })
+                            .then(res => {
+                                if (res.data.status === 'disable') {
+                                    // window.alert('Your account has been locked. Please, contact the admin to unlock your account.');
+                                    setFormWarning('Your account has been locked. Please, contact the admin to unlock your account.');
+                                    setOpen(true);
+                                }
+                                // reset the counter
+                                setCounter(0);
+                            })
+                            .catch(err => {
+                                window.alert(err.response.data.error);
 
+                            });
+                    }
+
+                });
+        }
     };
+
     return (
         <>
-            <h1>Login Page</h1>
+
+            <div className="bg-darkzero h-screen w-screen">
+                <div className="text-3xl p-2 font-bold">Welcome to Samaan Kinam E-commerce</div>
+                <div className="w-[80%] mx-auto" align="center">
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <div className="mx-auto pt-10">
+                                <img src="https://img.freepik.com/free-vector/ecommerce-checkout-laptop-concept-illustration_114360-8243.jpg?size=626&ext=jpg&ga=GA1.1.1078491387.1698994615&semt=ais" alt="" />
+                            </div>
+
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className='mx-auto pt-10'>
+
+                                <div style={{
+                                    border: '1px solid green',
+                                    boxShadow: "0 0 50px rgb(26, 176, 23) ",
+
+                                }}
+                                    className="rounded-lg mt-3 text-white bg-indigo-500 p-5 m-auto lg:w-[500px] md:w-[400px] sm:w-[300px]"
+                                    align="center"
+
+                                >
+
+                                    <div className="text-3xl font-bold">
+                                        LOGIN
+                                    </div>
+
+                                    <div className="mt-5">
+                                        <div className="mt-3 mb-2" align="left">
+                                            Email:
+                                        </div>
+                                        <input
+                                            type="email"
+                                            onChange={(e) => setCredentials({ ...credentials, email: e.target.value })}
+
+                                            placeholder="Enter Your Email ..."
+
+                                            onBlur={handleEmailError}
+                                            className={`input input-bordered 
+                                                    ${emailError ? "input-error" : "input-accent"}
+                                                     w-full`}
+
+                                        />
+                                    </div>
+                                    <div className="mt-5">
+                                        <div className="mt-3 mb-2" align="left">
+                                            Password:
+                                        </div>
+                                        <input
+                                            type="password"
+                                            onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+
+                                            placeholder="Enter Your Password ..."
+                                            onBlur={handlePasswordError}
+                                            className={`input input-bordered 
+                                                    ${passwordError ? "input-error" : "input-accent"}
+                                                    w-full`}
+
+                                        />
+                                    </div>
+
+                                    {
+                                        formError !== "" ? (
+                                            <div className="alert alert-error mt-4">
+                                                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                                                <span>{formError}</span>
+                                            </div>
+                                        ) : (null)
+                                    }
+
+                                    {
+                                        formWarning !== "" && (
+                                            <div className="warning-section mt-4">
+                                                <div className="alert alert-warning">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+                                                    <span>{formWarning}</span>
+                                                </div>
+                                            </div>
+                                        )
+                                    }
+
+                                    <button onClick={handleLogin} className='btn btn-primary w-full font-bold mt-8 mb-8'>LOGIN</button>
+
+
+                                    <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+                                        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+                                            Error: {formError}
+                                        </Alert>
+                                    </Snackbar>
+
+                                    <label htmlFor="" className='p-4'>
+                                        Don't have an account ?
+                                    </label>
+                                    <Chip
+                                        Avatar={<Avatar>R</Avatar>}
+                                        color="success"
+                                        label="REGISTER YOUR ACCOUNT"
+                                        onClick={() => navigate('/signup')}
+
+                                    ></Chip>
+
+                                </div>
+
+                            </div>
+                        </Grid>
+
+                    </Grid>
+                </div>
+
+
+            </div>
+            {/* <h1>Login Page</h1>
 
             <form action="" onSubmit={handleLogin}>
                 <label htmlFor="">Email:</label>
@@ -86,7 +254,7 @@ function Login() {
                 <br />
                 <input type="submit" value="Login" />
 
-            </form>
+            </form> */}
         </>
     );
 }
