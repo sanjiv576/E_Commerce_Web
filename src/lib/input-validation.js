b015ca24ed84a51d4a1a181e773deedc8831fa84
sanjiv576 <shresthasanjiv576@gmail.com>
2023-10-20 15:51:50 +0545

    add email and password validation

---
+++ src/lib/input-validation.js
@@ -0,0 +1,17 @@
+
+
+export const isEmail = (email) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+
+export const allLetter = (text) => {
+    if (text.trim().length < 5 || text.trim().length > 25) {
+        return false;
+    }
+    return true;
+}
+
+export const checkPassword = (password) => {
+    if (/^[A-Za-z]\w{7,14}$/.test(password.trim())) {
+        return true;
+    }
+    return false;
+};
