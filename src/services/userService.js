aa66316ff9999f9b2f071925cf90a9cb30215801
sanjiv576 <shresthasanjiv576@gmail.com>
2023-10-28 22:42:01 +0545

    get user data

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const baseUrl = 'http://localhost:3005/users';
+const getToken = () => `bearer ${window.localStorage.getItem('token')}`;
 
 const login = (loginCredentials) => {
     return axios.post(`${baseUrl}/login`, loginCredentials)
@@ -14,11 +15,21 @@ const passwordNeedChange = () => {
     return axios.post(`${baseUrl}/passwordNeedChange`, {});
 }
 
+const getUser = () => {
+    return axios.get(`${baseUrl}`, {
+        headers: {
+            Authorization: getToken()
+        }
+
+    });
+}
+
 
 const userServices = {
     login,
     lockAccount,
     passwordNeedChange,
+    getUser,
 
 }
 
