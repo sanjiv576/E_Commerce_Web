49f7964d5f1b76032a10294ea7207360b048a4b4
sanjiv576 <shresthasanjiv576@gmail.com>
2023-10-28 22:41:49 +0545

    complete homepage UIs

--- src/components/User/HomePage.jsx
+++ src/components/User/HomePage.jsx
@@ -1,20 +1,32 @@
 // only for registered users
 import { useEffect, useState } from "react";
 import dummyData from "../../data/data";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { usePurchase } from "../../utils/purchaseContext";
 import productServices from "../../services/productService";
 
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import { Button } from "@mui/material";
+import { ResponsiveAppBarHomepage } from "../AppBar/ResponsiveAppBarHomepage";
 
 function HomePage() {
     const purchase = usePurchase();
+    const navigate = useNavigate();
 
     const [purchaseProduct, setPurchaseProduct] = useState({});
     const [products, setProducts] = useState([]);
+    const [newReleasedProduct, setNewReleasedProduct] = useState({});
+    const [hottestProduct, setHottestProduct] = useState({});
+    const [userId, setUserId] = useState('');
 
     useEffect(() => {
         console.log(`Purchase context length from homepage is : ${purchase.purchase.length}`);
+
+
+        // set the purchase product in the state by reading from the local storage
+        setUserId(window.localStorage.getItem('userId'));
 
         setPurchaseProduct(
             {
@@ -35,6 +47,10 @@ function HomePage() {
                 res.data.forEach(product => {
                     console.log(`Product name from home: ${product.name}`);
                 });
+
+                // set the first and last product in the state
+                setNewReleasedProduct(res.data[0]);
+                setHottestProduct(res.data[res.data.length - 1]);
             })
             .catch(err => window.alert(err.response.data.error));
     }, []);
@@ -60,9 +76,6 @@ function HomePage() {
 
         }
     };
-
-
-
 
     // this works fine
     const handlePurchase = (e) => {
@@ -97,6 +110,127 @@ function HomePage() {
     const handleView = () => { };
     return (
         <>
+            <ResponsiveAppBarHomepage purchaseProductLength={purchase.purchase.length} />
+            <div className="text-3xl p-2 font-bold">Welcome to Samaan Kinam E-commerce</div>
+            <div className="carousel w-full h-90 carousel-section">
+                <div id="item1" className="carousel-item w-full">
+                    <img src="/images/4.png" className="w-full" />
+
+                </div>
+                <div id="item2" className="carousel-item w-full">
+                    <img src="/images/3.png" className="w-full" />
+
+                </div>
+                <div id="item3" className="carousel-item w-full">
+                    <img src="/images/5.png" className="w-full" />
+
+                </div>
+                <div id="item4" className="carousel-item w-full">
+                    <img src="/images/2.png" className="w-full" />
+
+                </div>
+            </div>
+            <div className="flex justify-center w-full py-2 gap-2">
+                <a href="#item1" className="btn btn-xs btn-secondary">1</a>
+                <a href="#item2" className="btn btn-xs btn-secondary">2</a>
+                <a href="#item3" className="btn btn-xs btn-secondary">3</a>
+                <a href="#item4" className="btn btn-xs btn-secondary">4</a>
+            </div>
+
+
+            <div className="carousel carousel-end rounded-box row-carousel mt-4">
+
+                {
+                    [...Array(5)].map((_, i) => {
+                        return products.map((product) => {
+                            return (
+
+                                <div className="carousel-item mr-5" key={product.id + i}>
+                                    <img src={`http://localhost:3005/product/${product.picture}`} alt="Drink" />
+                                </div>
+
+                            );
+                        })
+                    })
+                }
+
+            </div>
+
+            <div className="card lg:card-side bg-base-100 shadow-xl single-row-card">
+                <figure><img src={`http://localhost:3005/product/${newReleasedProduct.picture}`} /></figure>
+                <div className="card-body">
+                    <h2 className="card-title text-4xl font-bold mb-4">New released!</h2>
+                    <p className="justify-center">{newReleasedProduct.description}</p>
+                    <div className="card-actions justify-end">
+
+                        <Button onClick={() => navigate(`/singleProduct/${newReleasedProduct.id}`)} variant="contained" startIcon={<AddShoppingCartIcon />}>
+                            Add to cart
+                        </Button>
+                    </div>
+                </div>
+
+            </div>
+
+            <h2 className="text-5xl font-bold m-4">All Products</h2>
+
+
+            <div className="allProductsView">
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                    {
+                        [...Array(4)].map((_, i) => {
+
+                            return products.map((product) => (
+                                <div key={product.id} className="card bg-base-100 shadow-xl m-4">
+                                    <figure>
+                                        <img className="mt-4" src={`http://localhost:3005/product/${product.picture}`} alt="Product" />
+                                    </figure>
+                                    <div className="card-body">
+                                        <h2>{product.name}</h2>
+                                        <p>{product.description.substring(0, 99)}...</p>
+                                        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1">
+                                            {/* <Button className="mb-4" onClick={() => navigate(`/singleProduct/${product.id}`)} variant="contained" startIcon={<VisibilityIcon />}>
+                                                View
+                                            </Button> */}
+
+                                            <br />
+
+                                            <Button className="m-4" onClick={() => navigate(`/singleProduct/${product.id}`)} variant="contained" startIcon={<AddShoppingCartIcon />}>
+                                                Add to cart
+                                            </Button>
+                                        </div>
+                                        <div className="card-actions justify-end mt-2">
+                                            <div className="badge badge-outline badge-info">{product.category}</div>
+                                            <div className="badge badge-outline badge-info">Available</div>
+                                            <div className="badge badge-outline badge-info">Rs {product.price}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+
+
+                        })
+                    }
+                </div>
+            </div>
+
+            <div className="hero bg-base-200 mb-8">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+
+                    <img id="right-card" src={`http://localhost:3005/product/${hottestProduct.picture}`} className="max-w-sm rounded-lg shadow-2xl" />
+                    <div className="single-card-right">
+                        <h1 className="text-4xl font-bold">Hottest!</h1>
+                        <p className="py-6">{hottestProduct.description}</p>
+                        <div className="card-actions justify-start">
+                            <Button onClick={() => navigate(`/singleProduct/${hottestProduct.id}`)} variant="contained" startIcon={<AddShoppingCartIcon />}>
+                                Add to cart
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
             <h1>Home Page</h1>
 
             <p>NOte: Registered Users mode only</p>
