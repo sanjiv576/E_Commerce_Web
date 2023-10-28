3c417c09a8aaca74cc6415d3d2523a2545294ade
sanjiv576 <shresthasanjiv576@gmail.com>
2023-10-28 22:41:20 +0545

    add to cart ui

--- src/components/AppBar/ResponsiveAppBarHomepage.jsx
+++ src/components/AppBar/ResponsiveAppBarHomepage.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import userServices from '../../services/userService';
+import { useNavigate } from 'react-router-dom';
 
-export const ResponsiveAppBarHomepage = () => {
+export const ResponsiveAppBarHomepage = ({ purchaseProductLength }) => {
+
+    const [user, setUser] = useState({});
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        console.log(`Total purchase product length from appbar: ${purchaseProductLength}`);
+        userServices.getUser()
+            .then(res => setUser(res.data))
+            .catch(err => window.alert(err.response.data.error));
+    }, []);
+
+    const handleViewCart = () => {
+        navigate('/purchaseCart');
+    };
     return (
         <div>
             <div className="navbar bg-base-100">
                 <div className="flex-1">
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <a className="btn btn-ghost normal-case text-xl">Hi, {user.fullName}</a>
                 </div>
                 <div className="flex-none">
                     <div className="dropdown dropdown-end">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle">
-                            <div className="indicator">
+                        <label className="btn btn-ghost btn-circle">
+                            <div className="indicator" onClick={handleViewCart}>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                                <span className="badge badge-sm indicator-item">8</span>
+                                <span className="badge badge-sm indicator-item">{purchaseProductLength}</span>
                             </div>
                         </label>
-                        <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+                        {/* <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
                             <div className="card-body">
                                 <span className="font-bold text-lg">8 Items</span>
                                 <span className="text-info">Subtotal: $999</span>
@@ -23,12 +39,12 @@ export const ResponsiveAppBarHomepage = () => {
                                     <button className="btn btn-primary btn-block">View cart</button>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                     <div className="dropdown dropdown-end">
                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
-                                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                <img src={`http://localhost:3005/profile/${user.picture}`} />
                             </div>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
