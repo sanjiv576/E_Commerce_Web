62ef46abc9ec024ee4c0674c360daa9b826bac09
sanjiv576 <shresthasanjiv576@gmail.com>
2023-11-01 11:18:08 +0545

    review delete, update and add features included

--- src/services/productService.js
+++ src/services/productService.js
@@ -3,6 +3,7 @@
 import axios from "axios";
 const baseUrl = 'http://localhost:3005/products';
 const getToken = () => `bearer ${window.localStorage.getItem('token')}`;
+
 const getAllProudcts = () => {
     return axios.get(`${baseUrl}`);
 };
@@ -16,6 +17,28 @@ const getAllReviews = (id) => {
     return axios.get(`${baseUrl}/reviews/${id}`);
 
 }
+
+const addReview = (productId, review) => {
+    return axios.post(`${baseUrl}/reviews/${productId}`, review, {
+        headers: {
+            Authorization: getToken()
+        }
+    });
+};
+
+const deleteReview = (productId, reviewId) => {
+    return axios.delete(`http://localhost:3005/${productId}/${reviewId}`, {
+        headers: {
+            Authorization: getToken()
+        }
+    })
+};
+
+const updateReview = (productId, reviewId, updatedReview) => {
+    return axios.put(`http://localhost:3005/${productId}/${reviewId}`, updatedReview, {
+        headers: { Authorization: getToken() }
+    })
+};
 
 const purchaseProduct = (products) => {
     return axios.post('http://localhost:3005/purchase', products, {
@@ -31,6 +54,9 @@ const productServices = {
     getSingleProductById,
     getAllReviews,
     purchaseProduct,
+    addReview,
+    updateReview,
+    deleteReview,
 }
 
 export default productServices;
