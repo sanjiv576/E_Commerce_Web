e777a668b9c82fa256337f432404bcd6eb97ca06
sanjiv576 <shresthasanjiv576@gmail.com>
2023-11-03 11:13:22 +0545

    add register, delete account, upload images, change email features

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,6 +3,10 @@ import axios from "axios";
 const baseUrl = 'http://localhost:3005/users';
 const getToken = () => `bearer ${window.localStorage.getItem('token')}`;
 
+const register = (userData) => {
+    return axios.post(`${baseUrl}/register`, userData);
+
+}
 const login = (loginCredentials) => {
     return axios.post(`${baseUrl}/login`, loginCredentials)
 };
@@ -22,7 +26,62 @@ const getUser = () => {
         }
 
     });
-}
+};
+
+const deleteAccount = () => {
+    return axios.delete(`${baseUrl}`, {
+        headers: {
+            Authorization: getToken()
+        }
+    });
+};
+
+const changeEmail = (newEmail) => {
+    return axios.put(`${baseUrl}`, newEmail, {
+        headers: {
+            Authorization: getToken()
+        }
+    });
+
+};
+
+const changePassword = (newPassword) => {
+    return axios.put(`${baseUrl}/changePassword`, newPassword, {
+        headers: {
+            Authorization: getToken()
+        }
+    });
+};
+
+
+// upload file or image for profile
+const uploadProfileImage = (selectedImageFile) => {
+    const formData = new FormData();
+    formData.append('photo', selectedImageFile);
+
+    const config = {
+        headers: {
+            Authorization: getToken(),
+            'Content-Type': 'multipart/form-data'
+        }
+    };
+
+    return axios.post('http://localhost:3005/uploads', formData, config);
+};
+// upload file or image for profile
+const uploadProductImage = (productId, selectedImageFile) => {
+    const formData = new FormData();
+    formData.append('photo', selectedImageFile);
+
+    const config = {
+        headers: {
+            Authorization: getToken(),
+            'Content-Type': 'multipart/form-data'
+        }
+    };
+
+    return axios.post(`http://localhost:3005/uploads/${productId}`, formData, config);
+};
 
 
 const userServices = {
@@ -30,6 +89,12 @@ const userServices = {
     lockAccount,
     passwordNeedChange,
     getUser,
+    register,
+    deleteAccount,
+    changeEmail,
+    changePassword,
+    uploadProductImage,
+    uploadProfileImage,
 
 }
 
