a12f286d4f6bd99ed1b0ab91521a818994800845
sanjiv576 <shresthasanjiv576@gmail.com>
2023-11-04 10:10:36 +0545

    add dashboard and purchase history paths

--- src/components/AppBar/ResponsiveAppBarHomepage.jsx
+++ src/components/AppBar/ResponsiveAppBarHomepage.jsx
@@ -54,14 +54,13 @@ export const ResponsiveAppBarHomepage = ({ purchaseProductLength }) => {
                             </div>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><NavLink to={'/home'}>Dashboard</NavLink></li>
                             <li>
                                 <NavLink to={'/profile'}>Profile</NavLink>
 
                             </li>
-                            <li><a>Purchase History</a></li>
+                            <li><NavLink to={'/purchaseHistory'}>Purchase History</NavLink></li>
                             <li><a onClick={handleLogout}>Logout</a></li>
-
-
                         </ul>
                     </div>
                 </div>
